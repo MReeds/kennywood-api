ace710d2b32458cff12be258ca2a6ea52c4f6990
MReeds <mattlreeds@gmail.com>
2020-05-20 16:12:10 -0500

    Solved bug for itinerary view. Works in postman

--- kennywoodapi/models/attraction.py
+++ kennywoodapi/models/attraction.py
@@ -1,15 +1,14 @@
 from django.db import models
 from .parkarea import ParkArea
 
+
 class Attraction(models.Model):
 
     name = models.CharField(max_length=50)
-    area = models.ForeignKey(ParkArea, on_delete=models.DO_NOTHING, related_name="attractions")
+    area = models.ForeignKey(ParkArea, related_name="attractions", on_delete=models.DO_NOTHING)
 
     class Meta:
-        verbose_name = ("attraction")
-        verbose_name_plural = ("attractions")
+        ordering = ("area",)
 
     def __str__(self):
         return self.name
-
